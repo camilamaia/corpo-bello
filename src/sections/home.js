043ba47e7d3dd3d1bca68436c1012bc9a48fe4cc
Camila Maia <cmaiacd@gmail.com>
2017-09-10 13:53:18 -0300

    Home content

--- src/sections/home.js
+++ src/sections/home.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+import '../style/sections/home.css';
+
 class Home extends Component {
   render() {
     return (
       <div className="home">
-      	Nós
+      	<div className="title-banner">
+	      	<h1>Centro de Estética e Massoterapia</h1>
+	      	<h3>
+	      		<span>Massagens Relaxante, Terapêutica e Tântrica</span><br/>
+	      		<span>Depilação Masculina e Feminina</span>
+	      	</h3>
+	  	</div>
+	  	<div className="private-parking-banner">
+	  		<h4>Estacionamento privativo</h4>
+	  	</div>
+	  	<div className="contact">
+	  		(48) 3307.5497 | (48) 99600.0870 | (48) 99147.5169
+	  	</div>   
+	  	<div className="adress">
+	  		AV. Engenheiro Max de Souza Nº906 | Coqueiros - Florianópolis
+	  	</div>   
       </div>
     );
   }
