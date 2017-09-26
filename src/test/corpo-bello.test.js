778f6486b420a206fc45dee7aee17e4269df0438
Camila Maia <cmaiacd@gmail.com>
2017-09-26 10:32:25 -0300

    Updates

--- src/test/corpo-bello.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import CorpoBello from './corpo-bello';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CorpoBello />, div);
-});
